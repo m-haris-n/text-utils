8de6279a4024bebedb839cb7dfedc81532bf32b1
M. Haris Nabeel <m.haris.nabeel@gmail.com>
2022-06-20 23:38:57 -0700

    Updates

---
+++ precache-manifest.d9ed73610ba5db774124fd85822e8e29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0432bea6e3a94c37a3b8",
+    "url": "/text-utils/static/css/main.33aafef7.chunk.css"
+  },
+  {
+    "revision": "0432bea6e3a94c37a3b8",
+    "url": "/text-utils/static/js/main.0432bea6.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/text-utils/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "5e358973bcb7e154ea18",
+    "url": "/text-utils/static/js/2.5e358973.chunk.js"
+  },
+  {
+    "revision": "eef656731becf2616f54",
+    "url": "/text-utils/static/js/runtime~main.eef65673.js"
+  },
+  {
+    "revision": "2e9531061ba28a5190a46be8710eafdf",
+    "url": "/text-utils/index.html"
+  }
+];
